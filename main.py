9491109b4a515371401a6798df8ad9c5cdbe262e
DaneelOlivaw <2265970279@qq.com>
2024-08-28 13:59:33 +0800

    修复了opencv颜色空间转换问题，会导致识别不出来

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 from pathlib import Path
 import concurrent.futures
 import torch
+import logging
+import traceback
+from PIL import Image
 
 from split import split_videos
 
@@ -15,11 +18,13 @@ from split import split_videos
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 使用第一个GPU
 
 MODEL_NAME = "best"
-
+CONF = 0.2
+DEBUG = False
 video_folder = "videos"
 split_video_folder = "split_videos"
 output_base_folder = "output_frames"
-
+debug_output_folder = "debug_output"
+total_frame_count = 0
 
 
 # 加载YOLO模型
@@ -39,18 +44,37 @@ split_videos(video_folder, split_video_folder)
 # 获取视频文件夹中的所有视频文件
 video_files = [f for f in os.listdir(split_video_folder) if f.endswith(('.mp4', '.avi', '.mov'))]
 
-def process_batch(frames, frame_indices, output_folder):
-    with torch.no_grad():
-        results = engine_model(frames, conf=0.2, device=0)
+def process_frame(frame, frame_index, output_folder):
     
-    for i, result in enumerate(results):
-        if result.boxes.shape[0] > 0:
-            output_path = os.path.join(output_folder, f"frame_{frame_indices[i]:04d}.jpg")
-            cv2.imwrite(output_path, cv2.cvtColor(frames[i], cv2.COLOR_RGB2BGR))
+
+    global total_frame_count
+    try:
+        with torch.no_grad():
+            results = engine_model(frame, conf=CONF, device=0)
+            if results and len(results) > 0 and len(results[0].boxes.cls) > 0:
+                output_path = os.path.join(output_folder, f"frame_{total_frame_count}.jpg")
+                # print(f"检测到目标，输出路径: {output_path}")
+                
+                # 使用PIL保存图像
+                cv2.imwrite(output_path, frame)
+                
+                total_frame_count += 1
+            else:
+                # print(f"帧 {frame_index} 未检测到目标")
+                pass
+            
+            if DEBUG:   
+                debug_path = os.path.join(debug_output_folder, f"frame_{frame_index:04d}.jpg")
+                cv2.imwrite(debug_path, frame)
+
+    except Exception as e:
+        print(f"处理帧 {frame_index} 时发生错误: {str(e)}")
+        import traceback
+        print(traceback.format_exc())
 
 def process_video(video_file):
     video_path = os.path.join(split_video_folder, video_file)
-    output_folder = os.path.join(output_base_folder, os.path.splitext(video_file)[0])
+    output_folder = os.path.join(output_base_folder)
     os.makedirs(output_folder, exist_ok=True)
 
     cap = cv2.VideoCapture(video_path)
@@ -58,36 +82,29 @@ def process_video(video_file):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    target_size = 640
     start_x = (width - 640) // 2
     start_y = (height - 640) // 2
     end_x = start_x + 640
     end_y = start_y + 640
 
-    batch_size = 1  # 设置为1以匹配模型期望的输入
-    frames = []
-    frame_indices = []
-
     for frame_count in tqdm(range(total_frames), desc=f"处理 {video_file}", unit="帧"):
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        center_frame = frame[start_y:end_y, start_x:end_x]
-        center_frame_rgb = cv2.cvtColor(center_frame, cv2.COLOR_BGR2RGB)
-        frames.append(center_frame_rgb)
-        frame_indices.append(frame_count)
-
-        if len(frames) == batch_size:
-            process_batch(frames, frame_indices, output_folder)
-            frames = []
-            frame_indices = []
-
-    # 处理剩余的帧
-    if frames:
-        process_batch(frames, frame_indices, output_folder)
+        try:
+            ret, frame = cap.read()
+            if not ret:
+                logging.warning(f"无法读取帧 {frame_count}")
+                break
+            
+            # 裁剪并调整大小
+            center_frame = frame[start_y:end_y, start_x:end_x]           
+            process_frame(center_frame, frame_count, output_folder)
+        except Exception as e:
+            logging.error(f"处理视频 {video_file} 的帧 {frame_count} 时发生错误: {str(e)}")
+            logging.error(traceback.format_exc())
 
     cap.release()
-    print(f"处理完成: {video_file}")
+    cv2.destroyAllWindows()
+    logging.info(f"处理完成: {video_file}")
 
 # 使用线程池并行处理视频
 with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@ from ultralytics import YOLO
 import torch
 
 MODEL_NAME = "best"
+conf = 0.05
 model = YOLO(f"{MODEL_NAME}.pt")
 
 # 导出TensorRT模型
@@ -15,25 +16,31 @@ if not engine_model_path.exists():
 engine_model = YOLO(f"{MODEL_NAME}.engine")
 
 # 准备测试图像
-image_path = "test/1.jpg"  # 替换为您的图像路径
+image_path = r"C:\Users\daneel\Documents\python\extract_frame_from_video\debug_output\frame_1233.jpg"  # 替换为您的图像路径
 
 # 使用OpenVINO模型进行推理
 ov_model = YOLO(f"{MODEL_NAME}_openvino_model/")
-ov_results = ov_model(image_path)
+ov_results = ov_model(image_path, conf=conf)
 
 # 使用NCNN模型进行推理
 ncnn_model = YOLO(f"{MODEL_NAME}_ncnn_model/")
-ncnn_results = ncnn_model(image_path)
+ncnn_results = ncnn_model(image_path, conf=conf)
 
 # 使用TensorRT模型进行推理
-engine_results = engine_model(image_path)
+engine_results = engine_model(image_path, conf=conf)
 
 # 打印结果比较
 print("OpenVINO 结果:")
 print(ov_results[0].speed)
+print(len(ov_results[0].boxes.cls))
+print(ov_results[0].boxes)
 
 print("\nNCNN 结果:")
 print(ncnn_results[0].speed)
+print(len(ncnn_results[0].boxes.cls))
+print(ncnn_results[0].boxes)
 
 print("\nTensorRT 结果:")
 print(engine_results[0].speed)
+print(len(engine_results[0].boxes.cls))
+print(engine_results[0].boxes)
